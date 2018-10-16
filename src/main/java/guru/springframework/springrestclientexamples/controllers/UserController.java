e54a67c1024fbb9d4608d67fb84bb31676a7adb3
Elti Shehu <shehuelti@gmail.com>
2018-10-16 18:01:52 +0200

    Going Reactive with Spring WebClient

--- src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
+++ src/main/java/guru/springframework/springrestclientexamples/controllers/UserController.java
@@ -4,7 +4,6 @@ import guru.springframework.springrestclientexamples.services.ApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ServerWebExchange;
@@ -30,7 +29,7 @@ public class UserController {
     @PostMapping("/users")
     public String formPost(Model model, ServerWebExchange serverWebExchange) {
 
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+        /*MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
 
         Integer limit = new Integer(map.get("limit").get(0));
 
@@ -42,7 +41,13 @@ public class UserController {
             limit = 10;
         }
 
-        model.addAttribute("users", apiService.getUsers(limit));
+        model.addAttribute("users", apiService.getUsers(limit));*/
+
+        model.addAttribute("users",
+                apiService
+                        .getUsers(serverWebExchange
+                                .getFormData()
+                                .map(data -> new Integer(data.getFirst("limit")))));
 
         return "userlist";
     }
